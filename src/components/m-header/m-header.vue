96f81b97747885858f94ceded4f7d5df4a449b23
wangzhenxiao <wangzhenxiao@h3c.com>
2018-03-08 18:01:30 +0800

    'init'

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~common/stylus/variable';
+  @import '~common/stylus/mixin';
+
+    .my-header
+      position: relative
+      height: 44px
+      text-align: center
+      color: $color-theme
+      fontsiz: 0
+      .icon
+        display: inline-block
+        vertical-align: top
+        margin-right: 6px
+        width: 30px
+        height: 32px
+        margin-right: 9px
+        bg-image('logo')
+        background-size: 30px 32px
+      .text
+        display: inline-block
+        vertical-align: top
+        line-height: 44px
+        font-size: $font-size-large
+</style>
